2f92e4a63e919bf63dd4c9ff966aab3ca1b907d0
ermeifu <ermeifu2299@163.com>
2022-02-26 20:05:50 +0800

    2022-02-26 2016

--- 2016-MaximumDifferenceBetweenIncreasingElements/Solution.cpp
+++ 2016-MaximumDifferenceBetweenIncreasingElements/Solution.cpp
@@ -1,1 +1,17 @@
-class
+class Solution {
+public:
+	int maximumDifference(vector<int>& nums) {
+		int ans = -1;
+		int temp = nums[0];
+
+		for (int i = 1; i < nums.size(); ++i) {
+			if (nums[i] > temp) {
+				ans = max(ans, nums[i] - temp);
+			} else {
+				temp = nums[i];
+			}
+		}
+
+		return ans;
+	}
+};
